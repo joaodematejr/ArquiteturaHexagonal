e794d91d7f3f5625c8da143efaf6660428036224
joaodematejr <joao.demate@alquimidiacorp.com.br>
2023-09-01 07:01:49 -0300

    10. Criando o controller

---
+++ src/main/java/com/demate/hexagonal/adapters/in/controller/CustomerController.java
@@ -0,0 +1,2 @@
+package com.demate.hexagonal.adapters.in.controller;public class CustomerController {
+}
---
+++ src/main/java/com/demate/hexagonal/adapters/in/controller/request/CustomerRequest.java
@@ -0,0 +1,2 @@
+package com.demate.hexagonal.adapters.in.controller.request;public class CustomerRequest {
+}
